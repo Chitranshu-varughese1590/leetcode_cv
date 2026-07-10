540935f94e409932f898b78dc213283e16e643e4
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-07-10 15:21:13 +0530

    Update 0009-palindrome-number.py

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,24 @@
+# easy way 
 class Solution(object):
     def isPalindrome(self, x):
         s = str(x)
         if s == s[::-1]:
             return True
         else:
-            return False          
+            return False      
+
+
+
+#brute force 
+class solution(object):
+    def isPalindrome(self,x):
+        if x < 0 :
+            return False
+        num = x
+        palindrome = 0
+        while x > 0:
+            last = x % 10
+            palindrome = (palindrome * 10 ) + last
+            x = x // 10
+        return palindrome == num :
+            
