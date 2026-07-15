8673c81e8590586718a3c2595d63a3c187222afc
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-07-15 20:20:19 +0530

    Time: 680 ms (8.93%), Space: 12.4 MB (16.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,7 @@
-class Solution(object):
-    def func(self,n):
+class Solution(object): 
+    def fib(self, n):
         if n == 0 or n == 1:
             return n
-        return self.func(n-1) + self.func(n-2)    
-    def fib(self, n):
-        return self.func(n)
+        return self.fib(n-1) + self.fib(n-2)
+          
+        
