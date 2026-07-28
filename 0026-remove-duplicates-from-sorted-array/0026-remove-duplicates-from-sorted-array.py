694ae11cac2a8797eda0a5960ed793435672d977
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-07-28 19:36:39 +0530

    Time: 3 ms (58.46%), Space: 13.8 MB (70.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeDuplicates(self, nums):
+        if not nums:
+            return 0
         k = 1
 
         for i in range (1,len(nums)):
@@ -7,4 +9,4 @@ class Solution:
                 nums[k] = nums[i]
                 k +=1
 
-        return k    
+        return k   
