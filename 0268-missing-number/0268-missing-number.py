aa3002c409b3f77f9cd054b6d77b50dcb0fdbdf9
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-07-31 19:55:50 +0530

    Time: 0 ms (100%), Space: 13.4 MB (36.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def missingNumber(self, nums):
-        seen  = set(nums)    
+        n = len(nums)
 
-        for i in range (0,len(nums)+1):
-            if i not in seen:
-                return i
+        exp_sum = n*(n+1)//2
+        act_sum = sum(nums)
+
+        if exp_sum == act_sum:
+            return 0
+        else:
+            return exp_sum - act_sum    
+       
 
     
             
