ed537670378530f2d0f4c6363fcf82877380f300
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-08-06 19:42:15 +0530

    Time: 4 ms (79.49%), Space: 13.5 MB (49.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        if len(nums) == 0:
+            return nums
+        left = 0    
+        for i in range (len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[left]=nums[left],nums[i]
+                left+=1
+
+        return nums        
+            
+
+
