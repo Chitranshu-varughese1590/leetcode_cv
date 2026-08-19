e50c16ceb3fa693e8b57d8047976608901283cc9
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-08-19 10:17:57 +0530

    Time: 4 ms (48.23%), Space: 12.9 MB (95.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
         hash_map = {}
-        for i, num in enumerate(nums):
-            value  = target - num
+        for i,num in enumerate(nums):
+            value = target - num
             if value in hash_map:
                 return hash_map[value],i
             else:
-                hash_map[num] = i    
+                hash_map[num]=i      
         
