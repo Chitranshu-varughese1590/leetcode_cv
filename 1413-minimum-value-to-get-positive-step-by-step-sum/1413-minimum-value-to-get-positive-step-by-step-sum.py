577c3be9b5b15063fa0ba90747ffed41ba3c6d74
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-08-30 19:42:25 +0530

    Time: 0 ms (100%), Space: 12.4 MB (47.55%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minStartValue(self, nums):
+        total = 0
+        minimum = 0
+
+        for num in nums:
+            total += num
+            minimum = min(minimum, total)
+
+        return 1 - minimum
